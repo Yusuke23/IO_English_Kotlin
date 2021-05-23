567a1ed653eb66d0b48bef83d0e7c2c707c6c093
Yusuke Asai <f.a.ysk.08@gmail.com>
2021-05-23 14:27:01 +0900

    model change

---
+++ app/src/main/java/com/example/ioenglish/activities/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.ioenglish.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.SyncStateContract
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ioenglish.databinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity() {
+
+    companion object {
+        const val CREATE_BOARD_REQUEST_CODE: Int = 12
+    }
+
+    //todo ボタンを押した時の処理　mUserName
+    private lateinit var mUserName: String
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iAppBarMain.fabCreateBoard.setOnClickListener {
+            val intent = Intent(this, AddNotesActivity::class.java)
+            //todo ボタンを押した時の処理　mUserName
+//            intent.putExtra(SyncStateContract.Constants.NAME, mUserName)
+            startActivityForResult(intent, CREATE_BOARD_REQUEST_CODE)
+        }
+    }
+
+}
