390e5cf4f89e70f05002b047f9c21880a3e36e37
Yusuke Asai <f.a.ysk.08@gmail.com>
2021-05-25 12:07:11 +0900

    implement date picker & select image

--- app/src/main/java/com/example/ioenglish/activities/MainActivity.kt
+++ app/src/main/java/com/example/ioenglish/activities/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.ioenglish.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.example.ioenglish.R
 import com.example.ioenglish.databinding.ActivityMainBinding
+import com.example.ioenglish.utils.Constants
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    companion object {
-        const val CREATE_BOARD_REQUEST_CODE: Int = 12
-    }
-
-    //todo ボタンを押した時の処理　mUserName
-    private lateinit var mUserName: String
+    private lateinit var mSharedPreferences: SharedPreferences
 
     private lateinit var binding: ActivityMainBinding
 
@@ -21,11 +23,65 @@ class MainActivity: AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.iAppBarMain.fabCreateBoard.setOnClickListener {
-            val intent = Intent(this, AddNotesActivity::class.java)
-            //todo ボタンを押した時の処理　mUserName
-            startActivityForResult(intent, CREATE_BOARD_REQUEST_CODE)
+        // 右下のフローティングボタンを押すとノートを書くページへ遷移
+        binding.iAppBarMain.fabCreateNote.setOnClickListener {
+            startActivity(Intent(this, AddNotesActivity::class.java))
         }
+
+        setupActionBar()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        mSharedPreferences = this.getSharedPreferences(
+            Constants.IOENGLISH_PREFERENCES, Context.MODE_PRIVATE
+        )
+
+    }
+
+    // view binding 別のXMLから参照 /
+    private fun setupActionBar() {
+        setSupportActionBar(binding.iAppBarMain.toolbarMainActivity)
+        binding.iAppBarMain.toolbarMainActivity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        binding.iAppBarMain.toolbarMainActivity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+        } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                mSharedPreferences.edit().clear().apply()
+
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 
 }
